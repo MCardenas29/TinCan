24c66b2a415e910a25f2a1408e9eeb32c2b94721
Carlos Nolazco <carlos_sceptile@hotmail.com>
2023-05-28 22:11:11 -0600

    sisisi

---
+++ firebase/config.py
@@ -0,0 +1,17 @@
+#Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+#TODO: Add SDKs for Firebase products that you want to use
+#https://firebase.google.com/docs/web/setup#available-libraries
+
+#Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAkycVGdXgRUP1b_l-rWLu1NKAJdfiizPg",
+  authDomain: "yolotroll-6ef4a.firebaseapp.com",
+  projectId: "yolotroll-6ef4a",
+  storageBucket: "yolotroll-6ef4a.appspot.com",
+  messagingSenderId: "449150495549",
+  appId: "1:449150495549:web:7fe350906d0e2f07923354"
+};
+
+#Initialize Firebase
+const app = initializeApp(firebaseConfig);
