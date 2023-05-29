0381e79b98eef1d854e1c4f42b909ce7a9bc0a90
Madjes <Mauricio.291202@gmail.com>
2023-05-29 07:01:14 -0600

    merged

--- src/app.py
+++ src/app.py
@@ -1,6 +1,9 @@
 from flask import Flask
 from threading import Lock
+from firebase import firebase
 
+
+firebase = firebase.FirebaseApplication("https://yolotroll-6ef4a-default-rtdb.firebaseio.com", None)
 app = Flask(__name__)
 frames = [None, None]
 in_lock = Lock()
--- src/inference.py
+++ src/inference.py
@@ -4,6 +4,8 @@ import torch
 import cv2
 import src.app as app
 from threading import Event
+from firebase import firebase
+from datetime import datetime
 
 FPS = 30
 FRAME_SIZE = 640/2, 480/2
@@ -11,8 +13,17 @@ DEV_INDEX = 0
 
 # Thread main routine
 def main(event):
-    last_timestamp = time.time()
-    model = torch.hub.load('ultralytics/yolov5', 'custom', './can_recognition.pt')
+    init_time = time.time()
+    current_seg = 0
+    five_count = 0
+    minuts = 0
+    total_cans = 0
+    good_cans = 0
+    bad_cans = 0
+    good_prom = 0
+    bad_prom = 0
+    # last_timestamp = time.time()
+    model = torch.hub.load('ultralytics/yolov5', 'custom', './models/m1.onnx')
     camera = cv2.VideoCapture(DEV_INDEX)
     # Verify that the video device is succesfully opened
     if not camera.isOpened():
@@ -26,22 +37,64 @@ def main(event):
     # Process camera
     while not event.is_set():
         # Process the framerate
-        current_time = time.time()
-        if (current_time - last_timestamp) < (1/FPS):
-            continue
-        last_timestamp = current_time
+        # current_time = time.time()
+        # if (current_time - last_timestamp) < (1/FPS):
+        #     continue
+        # last_timestamp = current_time
         # Get the current frame
         ret, frame = camera.read()
         if not ret:
             continue
-        inference = model(frame)
-        inference.render()
+
+        diff = time.time() - init_time
+        frame = cv2.resize(frame, (640, 640))
+        res = model(frame)
+        res.render()
         # Write the current frame in ~disk~ ram
         # with app.in_lock:
         app.frames[0] = frame
-        app.frames[1] = inference.ims[0]
+        app.frames[1] = res.ims[0]
         # cv2.imwrite('current_frame.png', frame)
-        # cv2.imwrite('inference_frame.png', inference.ims[0])
+        # cv2.imwrite('res.png', res.ims[0])
+    if(diff >= 1):
+        init_time = time.time() - diff + 1
+        current_seg += 1
+        five_count += 1
+        for v in res.pandas().xyxy[0].get('name'):
+            total_cans += 1
+            if(v == 'OK'):
+                good_cans += 1
+            elif(v == 'BAD'):
+                bad_cans +=1
+
+    if(current_seg >= 60):
+        minuts += 1
+        current_seg = 0
+
+    if(minuts >= 1):
+        now = datetime.now()
+        good_prom = good_cans / total_cans
+        bad_prom = bad_cans / total_cans
+        print(good_prom)
+        print(bad_prom)
+        datos_g={
+            'promedio' : good_prom,
+            'fecha' : now
+        }
+        datos_b={
+            'promedio' : bad_prom,
+            'fecha' : now
+        }
+        god_res = firebase.post('/buenas', datos_g)
+        bad_res = firebase.post('/malas', datos_b)
+
+        total_cans = 0
+        good_cans = 0
+        bad_cans = 0
+        good_prom = 0
+        bad_prom = 0
+    minuts = 0
+
 
     camera.release()
     del(camera)
